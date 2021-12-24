fee497ac4f754786fbb22213df6a74cc0b275582
karnena <santoshkarnena95811@gmail.com>
2021-12-24 18:46:20 +0530

    react 360

--- web/src/components/ThreeD/index.js
+++ web/src/components/ThreeD/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useRef } from 'react';
 
 import Tridi from 'react-tridi';
+import ThreeSixty from 'react-360-view'
 import 'react-tridi/dist/index.css';
 
 
@@ -33,14 +34,15 @@ const ThreeD = () => {
 	}
 
 	return (
-		<div style={{ width: '500px' }}>
+		<>
+		{/* <div style={{ width: '500px' }}>
 			<Tridi
 				ref={tridiRef}
-				location="../images"
+				location="https://res.cloudinary.com/santoshk/image/upload/v1640325200/images"
 				format="jpg"
 				count="36"
 				onFrameChange={frameChangeHandler}
-				// autoplaySpeed={70}
+				autoplaySpeed={70}
 				onAutoplayStart={() => setIsAutoPlayRunning(true)}
 				onAutoplayStop={() => setIsAutoPlayRunning(false)}
 				onRecordStart={recordStartHandler}
@@ -62,7 +64,14 @@ const ThreeD = () => {
 			<button onClick={() => tridiRef.current.toggleAutoplay(!isAutoPlayRunning)}>
 				{isAutoPlayRunning ? 'Pause' : 'Autoplay'}
 			</button>
-		</div>
+		</div> */}
+		<ThreeSixty
+    amount={36}
+    imagePath="https://res.cloudinary.com/santoshk/image/upload/v1640325200/images"
+    fileName="{index}.jpg"
+/>
+</>
+
 	);
 };
 
